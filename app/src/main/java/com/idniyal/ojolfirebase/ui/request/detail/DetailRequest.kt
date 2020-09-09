2c824e96dc33d82ba8dd6715c7c2d6c133083749
iyaalfiryal <iyaalfiryal@gmail.com>
2020-09-09 15:07:36 +0700

    rabu siang

--- app/src/main/java/com/idniyal/ojolfirebase/ui/request/detail/DetailRequest.kt
+++ app/src/main/java/com/idniyal/ojolfirebase/ui/request/detail/DetailRequest.kt
@@ -1,24 +1,37 @@
 package com.idniyal.ojolfirebase.ui.request.detail
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.idniyal.ojolfirebase.MainActivity
 import com.idniyal.ojolfirebase.R
 import com.idniyal.ojolfirebase.model.Booking
 import com.idniyal.ojolfirebase.utils.Constan
+import com.idniyal.ojolfirebase.utils.Constan.key
 import kotlinx.android.synthetic.main.activity_detail_request.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
 
 class DetailRequest : AppCompatActivity(), OnMapReadyCallback {
 
     var status: Int? = null
     var booking: Booking? = null
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +56,23 @@ class DetailRequest : AppCompatActivity(), OnMapReadyCallback {
 
         detailBooking()
 
+        detail_button.onClick {
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference(Constan.tb_booking)
+
+            if (status == 1){
+                myRef.child(key).child("status").setValue(2)
+                myRef.child(key).child("driver")
+                    .setValue(auth.currentUser?.uid)
+
+                startActivity<MainActivity>()
+            }
+            else if(status == 2){
+                myRef.child(key).child("status").setValue(4)
+                startActivity<MainActivity>()
+            }
+        }
     }
 
     private fun detailBooking() {
@@ -71,7 +101,40 @@ class DetailRequest : AppCompatActivity(), OnMapReadyCallback {
         })
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
-        TODO("Not yet implemented")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val latlng = booking?.latAwal?.let{ booking?.lonAwal?.let{
+                it1 -> LatLng(it, it1) } }
+
+        val latlng1 = booking?.latTujuan?.let{ booking?.lonTujuan?.let{
+                it1 -> LatLng(it, it1) } }
+
+        val res = this.resources
+        val marker1 = BitmapFactory.decodeResource(res, R.mipmap.ic_pin)
+        val smallmarker = Bitmap.createScaledBitmap(marker1, 80,
+            120, false)
+
+        mMap.addMarker(latlng?.let{
+            MarkerOptions().position(it).title("awal")
+                .icon(BitmapDescriptorFactory.fromBitmap(smallmarker))
+        })
+        mMap.addMarker(latlng1?.let {
+            MarkerOptions().position(it).title("tujuan")
+                .icon(BitmapDescriptorFactory.defaultMarker(
+                    BitmapDescriptorFactory.HUE_RED))
+        })
+
+        //menampilkan marker secara kesuluruhan
+        val builder = LatLngBounds.builder()
+        builder.include(latlng)
+        builder.include(latlng1)
+
+        mMap.setOnCameraIdleListener {
+            mMap.moveCamera(
+                CameraUpdateFactory.newLatLngBounds(builder.build(), 32)
+            )
+        }
+
     }
 }
